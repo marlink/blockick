f14243e786c85a58188e92870bbea7168133507e
marlink <marceli.cieplik@gmail.com>
2025-08-29 10:25:30 +0200

    Fix TypeScript error in developerMenu.ts with type casting

--- src/scripts/objects/developerMenu.ts
+++ src/scripts/objects/developerMenu.ts
@@ -15,7 +15,7 @@ export default class DeveloperMenu extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y);
     // Fix the type error by using the scene's add method
-    scene.add.existing(this);
+    scene.add.existing(this as any);
     
     // Get the version manager instance
     this.versionManager = VersionManager.getInstance();
